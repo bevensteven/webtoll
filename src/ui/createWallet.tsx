7e373fd5a6c16b894b99b3dcdf9cc255675f06b4
bevensteven <struong@berkeley.edu>
2019-12-09 14:45:59 -0800

    fix: generate wallet from test faucet instead

--- src/ui/createWallet.tsx
+++ src/ui/createWallet.tsx
@@ -1,14 +1,32 @@
 import * as React from "react"
 import * as xpring from "xpring-js"
+import axios from 'axios'
+
+/**
+ * Retrieve a secret from the testnet faucet.
+ */
+async function seedFromFaucet() {
+    const faucetURL = 'https://faucet.altnet.rippletest.net/accounts'
+    try {
+        const resp = await axios.post(faucetURL)
+        if (resp.data.account.secret) {
+            return resp.data.account.secret
+        }
+        throw new Error('Could not generate seed from faucet.')
+    } catch (err) {
+        console.error(err)
+        throw new Error('Error: ' + err)
+    }
+}
 
 class CreateWallet extends React.Component {
 
     /**
      * Creates a user wallet and saves it to storage.
      */
-    handleClick = () => {
-        const result = xpring.Wallet.generateRandomWallet()
-        const newWallet = result.wallet
+    handleClick = async () => {
+        const seed = await seedFromFaucet()
+        const newWallet = xpring.Wallet.generateWalletFromSeed(seed)
         
         console.log(newWallet.getAddress())
         console.log(newWallet.getPublicKey())
@@ -16,13 +34,14 @@ class CreateWallet extends React.Component {
 
         let data = {
             wallet: {
-                publicKey: newWallet.getPublicKey,
-                privateKey: newWallet.getPrivateKey
+                publicKey: newWallet.getPublicKey(),
+                privateKey: newWallet.getPrivateKey()
             }
         }
         chrome.storage.sync.set(data, function() {
             // Notify that the addr has been saved
-            chrome.runtime.sendMessage('Created wallet and saved data to webtool: ', data)
+            chrome.runtime.sendMessage(`Created wallet with addr=${newWallet.getAddress()}`)
+            console.log('Saved wallet data to storage: ', data)
         })
     }
 
