c3bd35f04c7261ec3ca245551572a56346f9334a
bevensteven <struong@berkeley.edu>
2019-12-08 23:38:56 -0800

    refactor: move UrlInput into separate file

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,57 +1,8 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
+import "./UrlInput"
 import "../styles/popup.css"
-
-class UrlInput extends React.Component {
-    state: any;
-    constructor(props: any) {
-        super(props)
-        this.state = {value: ''}
-
-        // bind functions
-        this.updateStorage = this.updateStorage.bind(this)
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    /**
-     * Updates Chrome storage when user adds a URL.
-     * 
-     * TODO: add support for adding and storing multiple URLs.
-     */
-    updateStorage(url: string) {
-        // validate string
-        // add string to storage
-        chrome.storage.sync.set({'value': url}, function() {
-                // Notify that the URL has been saved
-                chrome.runtime.sendMessage(`${url} has been saved to webtoll`)
-        })
-        // handle error
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        alert('URL was submitted: ' + this.state.value);
-        this.updateStorage(this.state.value)
-        event.preventDefault();
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    URL:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        );
-    }
-}
 
 class Extension extends React.Component {
     render() {
